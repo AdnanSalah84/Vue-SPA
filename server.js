3dcf12df166bd4e09ef251209731bd02904146c6
adnansalah84 <adnan.salah84@gmail.com>
2018-10-12 12:30:06 +0500

    create spa project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const indexHTML = (()=>{
+  return fs.readFileSync(path.resolve(__dirname,'./index.html') , 'utf-8');
+})();
+
+app.get("*", (req,res)=>{
+  //res.write("Hey there");
+  res.write(indexHTML);
+  res.end();
+});
+
+const port = process.env.PORT || 30000;
+app.listen(port, () =>{
+  console.log(`server started at http://localhost:${port}`);
+})
